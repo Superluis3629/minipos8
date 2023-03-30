86d50c70c58bcd7733d0581bf6162c3e2f3cc692
Superluis3629 <lui.sengdara@gmail.com>
2023-03-30 09:05:14 +0700

    frist commit pc v2

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1> VueJS App V3 </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Minipos8App',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
